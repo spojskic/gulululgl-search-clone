3233cd90374d524c6fb86194ca38c0242544e3a6
MagicPojska <pojskicsafet@gmail.com>
2021-10-21 16:32:14 +0200

    Brought back description for searches

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -26,12 +26,15 @@ export const Results = () => {
         case '/search':
             return (
                 <div className="sm:px-56 flex flex-wrap justify-between space-y-6">
-                    {results?.results?.map(({ link, title }, index) => (
+                    {results?.results?.map(({ link, title, description }, index) => (
                         <div key={index} className="md:w-2/5 w-full">
                             <a href={link} target="_blank" rel="noreferrer">
                                 <p className="text-sm">{link.length > 30 ? link.substring(0, 30) : link}</p>
                                 <p className="text-lg hover:underline dark:text-blue-300 text-blue-700  ">{title}</p>
                             </a>
+                            <p className='text-sm'>
+                                {description}
+                            </p>
                         </div>
                     ))}
                 </div>
