a914ed35c988ff995154fb0b706b1705f42e09ae
MagicPojska <pojskicsafet@gmail.com>
2021-10-21 16:07:56 +0200

    Fixed errore when switching between All, News, Images and Videos sections by reverting to old style without if and else statements in our context

--- src/contexts/ResultsContextProvider.js
+++ src/contexts/ResultsContextProvider.js
@@ -17,20 +17,21 @@ export const ResultContextProvider = ({ children }) => {
       headers: {
         "x-user-agent": "desktop",
         "x-rapidapi-host": "google-search3.p.rapidapi.com",
-        "x-rapidapi-key": "fa94dacc43msh0bd2ce54684e0e6p1e54e6jsnecd1e2fd900e",
+        "x-rapidapi-key": process.env.REACT_APP_API_KEY,
       },
     });
     const data = await response.json();
 
     //Because API returns entries object name we need to rename it so it wont this we are calling entries javascirpt funciton
-    if (type.includes("/news")) {
-      setResults(data.entries);
-    } else if (type.includes("/images")) {
-      setResults(data.image_results);
-    } else {
-      setResults(data);
-    }
+    // if (type.includes("/news")) {
+    //   setResults(data.entries);
+    // } else if (type.includes("/images")) {
+    //   setResults(data.image_results);
+    // } else {
+    //   setResults(data.results);
+    // }
 
+    setResults(data);
     setIsLoading(false);
   };
 
