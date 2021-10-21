128ed3ed194652dd4c46b74c51b38ce4d1d37da7
MagicPojska <pojskicsafet@gmail.com>
2021-10-21 13:54:57 +0200

    Added search functionality for showing link,title and description

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -9,12 +9,39 @@ export const Results = () => {
     const { results, isLoading, getResults, searchTerm } = useResultContext()
     const location = useLocation()
 
+    useEffect(() => {
+        getResults('/search/q=JavaScript Mastery&num=40')
+    }, [])
+
     if (isLoading) return <Loading />
 
     switch (location.pathname) {
         case '/search':
+            return (
+                <div className='flex flex-wrap justify-between space-y-6 sm:px-56'>
+                    {results?.results?.map(({ link, title, description }, index) => (
+                        <div key={index} className='md:w-2/5'>
+                            <a href={link} target='_blank' rel='norefferer'>
+                                <p className='text-sm'>
+                                    {link.length > 30 ? link.substring(0, 30) : link}
+                                </p>
+                                <p className='text-lg hover:underline dark:text-blue-300 text-blue-700'>
+                                    {title}
+                                </p>
+                            </a>
+                            <p className='text-sm'>
+                                {description}
+                            </p>
+                        </div>
+                    ))}
+                </div>
+            )
+        case '/images':
             return 'Search'
-
+        case '/news':
+            return 'News'
+        case '/videos':
+            return 'Videos'
         default:
             return 'ERROR!';
     }
